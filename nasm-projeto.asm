329f670186fd76422be6674a349d2a8dfda87366
Ciro Callazans <ccallazans@protonmail.com>
2022-11-18 19:33:36 -0300

    questao 1 concluida

--- nasm-projeto.asm
+++ nasm-projeto.asm
@@ -1,14 +1,19 @@
 %include "io.inc"
 
 section .data
+    string1 db "Qual e a importancia da escola na democratizacao da sociedade", 0;
+    len_string1 equ $- string1
 
-section.bss
+section .bss
+    buffer_string1: resb len_string1
 
 section .text
 global CMAIN
 
 CMAIN:
-    mov ebp, esp; for correct debugging
+    mov ebp, esp    ; for correct debugging
+    xor eax, ebx
+    
     call questao_1
     call questao_2
     call questao_3
@@ -19,7 +24,19 @@ CMAIN:
     ret
 
 questao_1:
+    mov ecx, 41             ; Posição da string para parar o loop
+    mov esi, string1 + 7    ; Mover a "string1" deslocado de 7 bytes para recortar o "Qual e "
+    mov edi, buffer_string1 ; Mover o "buff_string1" para receber os bytes de "string1"
+    cld                     ; Setar zf=0 para incrementar as posições de esi e edi
+    rep movsb               ; Repetir a operação até ecx=0, "move string byte"
+    
+    PRINT_STRING buffer_string1
+    NEWLINE
     ret
+                    
+teste:
+    PRINT_DEC 2, ecx
+    PRINT_STRING 'x'
 
 questao_2:
     ret
