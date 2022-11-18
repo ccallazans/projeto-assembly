0d0bb28903d5d604b9cfb9fa884dfc16d107b887
Ciro Callazans <ccallazans@protonmail.com>
2022-11-18 12:01:27 -0300

    inicio

--- nasm-projeto.asm
+++ nasm-projeto.asm
@@ -1,9 +1,33 @@
 %include "io.inc"
 
+section .data
+
+section.bss
+
 section .text
 global CMAIN
+
 CMAIN:
     mov ebp, esp; for correct debugging
-    ;write your code here
-    xor eax, eax
+    call questao_1;
+    call questao_2;
+    call questao_3;
+    call questao_4;
+    call questao_5;
+    call questao_6;
+    call questao_7;
     ret
+
+questao_1:
+
+questao_2:
+
+questao_3:
+
+questao_4:
+
+questao_5:
+
+questao_6:
+
+questao_7:
