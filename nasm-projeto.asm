be7f5935f21e4c4580a01d038b70d7f13cc346c7
Ciro Callazans <ccallazans@protonmail.com>
2022-11-18 21:12:49 -0300

    questao 2 concluida

--- nasm-projeto.asm
+++ nasm-projeto.asm
@@ -3,6 +3,9 @@
 section .data
     string1 db "Qual e a importancia da escola na democratizacao da sociedade", 0;
     len_string1 equ $- string1
+    
+    msg_a db "a -> ", 0
+    msg_m db "m -> ", 0
 
 section .bss
     buffer_string1: resb len_string1
@@ -39,7 +42,39 @@ teste:
     PRINT_STRING 'x'
 
 questao_2:
+    mov ecx, 41 ; Tamanho da string
+    mov bl, 0   ; Zerar bl para contar 'm'
+    mov bh, 0   ; Zerar bh para contar 'a'
+    
+    loop2:
+        contar_m:
+            cmp byte [buffer_string1 + ecx], 'm'    ; Se o último caractere é igual a 'm'
+            jne contar_a    ; Caso contrário, pular para "contar_a"
+            
+            inc bl          ; Incrementar valor encontrado
+            jmp atualizar   ; Pular para "atualizar"
+            
+        contar_a:
+            cmp byte [buffer_string1 + ecx], 'a'    ; Se o último caractere é igual a 'a'
+            jne atualizar   ; Caso contrário, pular para "atualizar"
+            
+            inc bh          ; Incrementar valor encontrado
+            
+        atualizar:    
+            dec ecx     ; Decrementar eax, já que estamos lendo do final da string até o começo
+            cmp ecx, 0  ; Se ecx é igual a 0
+            jne loop2   ; Caso contrário, pular para "loop2"
+        
+    PRINT_STRING msg_m
+    PRINT_DEC 2, bl
+    NEWLINE  
+   
+    PRINT_STRING msg_a
+    PRINT_DEC 2, bh
+    NEWLINE  
+        
     ret
+
 
 questao_3:
     ret
